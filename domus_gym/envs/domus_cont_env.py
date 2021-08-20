correct normalisation to -1 -> 1

domus_gym/envs/domus_cont_env.py
@@ -46,9 +46,7 @@ class DomusContEnv(DomusEnv):
             ]
         )
         self.act_tr = MinMaxTransform(act_min, act_max)
-        self.action_space = spaces.Box(
-            high=1, low=0, shape=act_min.shape, dtype=np.float32
-        )
+        self.action_space = spaces.Box(high=1, low=-1)
 
     def _convert_action(self, action: np.ndarray):
         """given some action, convert it first into the controller state

domus_gym/envs/domus_env.py
@@ -163,7 +163,7 @@ class DomusEnv(gym.Env):
         self.obs_tr = MinMaxTransform(obs_min, obs_max)
 
         self.observation_space = spaces.Box(
-            high=1, low=0, shape=obs_min.shape, dtype=np.float32
+            high=1, low=-1, shape=obs_min.shape, dtype=np.float32
         )
         self.action_grid = [
             # blower_level 5, 10, or 18

domus_gym/envs/minmax.py
@@ -7,15 +7,15 @@ class MinMaxTransform:
         assert (max_value > min_value).all()
 
         self.min_value = min_value
-        self.max_minus_min = max_value - min_value
+        self.max_minus_min_on_2 = (max_value - min_value) / 2
 
     def transform(self, value):
         assert value.shape == self.min_value.shape
-        return np.divide(value - self.min_value, self.max_minus_min)
+        return np.divide(value - self.min_value, self.max_minus_min_on_2) - 1
 
     def inverse_transform(self, value):
         assert value.shape == self.min_value.shape
-        # x = (v - min) / (max - min)
-        # x * (max - min) = (v - min)
-        # min + x * (max - min) = v
-        return self.min_value + np.multiply(value, self.max_minus_min)
+        # x = 2 * (v - min) / (max - min) - 1
+        # (x + 1) * (max - min) / 2 = (v - min)
+        # min + (x + 1) * (max - min) / 2 = v
+        return self.min_value + np.multiply(value + 1, self.max_minus_min_on_2)

tests/test_minmax.py
@@ -3,6 +3,8 @@ from domus_gym.envs.minmax import MinMaxTransform
 import numpy as np
 from numpy.testing import assert_array_equal
 
+from pytest import approx
+
 
 def test_minmaxtransform():
     obs_min = np.array([100, 0, -100])
@@ -11,7 +13,11 @@ def test_minmaxtransform():
     tr = MinMaxTransform(obs_min, obs_max)
 
     v = np.array([110, 0.1, 0])
-    assert_array_equal(tr.inverse_transform(tr.transform(v)), v)
+    assert v == approx(tr.inverse_transform(tr.transform(v)))
     t = tr.transform(v)
-    assert (t >= np.zeros(obs_min.shape)).all()
+    assert (t >= -np.ones(obs_min.shape)).all()
     assert (t <= np.ones(obs_min.shape)).all()
+    assert tr.transform(obs_min) == approx(np.array([-1, -1, -1]))
+    assert tr.transform(obs_max) == approx(np.array([1, 1, 1]))
+    assert tr.transform(obs_min) == approx(-1)
+    assert tr.transform(obs_max) == approx(1)