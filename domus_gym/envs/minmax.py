78a5ce9aa68656e1c3a39da81217fca733d85340
James Brusey <james.brusey@gmail.com>
2022-06-24 09:27:06 +0100

    fix for box contains failure with gym 0.21.0
    Box object now considers a vector with a different dtype to be not
    contained within the space. I have fixed minmax.py but there may be
    other things that need to change.

--- domus_gym/envs/minmax.py
+++ domus_gym/envs/minmax.py
@@ -2,20 +2,26 @@ import numpy as np
 
 
 class MinMaxTransform:
-    def __init__(self, min_value, max_value):
+    def __init__(self, min_value, max_value, dtype=np.float32):
         assert max_value.shape == min_value.shape
         assert (max_value > min_value).all()
 
         self.min_value = min_value
         self.max_minus_min_on_2 = (max_value - min_value) / 2
+        self.dtype = dtype
 
     def transform(self, value):
         assert value.shape == self.min_value.shape
-        return np.divide(value - self.min_value, self.max_minus_min_on_2) - 1
+        return (
+            np.divide(value - self.min_value, self.max_minus_min_on_2, dtype=self.dtype)
+            - 1
+        )
 
     def inverse_transform(self, value):
         assert value.shape == self.min_value.shape
         # x = 2 * (v - min) / (max - min) - 1
         # (x + 1) * (max - min) / 2 = (v - min)
         # min + (x + 1) * (max - min) / 2 = v
-        return self.min_value + np.multiply(value + 1, self.max_minus_min_on_2)
+        return self.min_value + np.multiply(
+            value + 1, self.max_minus_min_on_2, dtype=self.dtype
+        )
--- tests/test_minmax.py
+++ tests/test_minmax.py
@@ -11,12 +11,13 @@ def test_minmaxtransform():
 
     tr = MinMaxTransform(obs_min, obs_max)
 
-    v = np.array([110, 0.1, 0])
+    v = np.array([110, 0.1, 0], dtype=np.float32)
     assert v == approx(tr.inverse_transform(tr.transform(v)))
     t = tr.transform(v)
-    assert (t >= -np.ones(obs_min.shape)).all()
-    assert (t <= np.ones(obs_min.shape)).all()
-    assert tr.transform(obs_min) == approx(np.array([-1, -1, -1]))
-    assert tr.transform(obs_max) == approx(np.array([1, 1, 1]))
+    assert t.dtype == np.float32
+    assert (t >= -np.ones(obs_min.shape, dtype=np.float32)).all()
+    assert (t <= np.ones(obs_min.shape, dtype=np.float32)).all()
+    assert tr.transform(obs_min) == approx(np.array([-1, -1, -1], dtype=np.float32))
+    assert tr.transform(obs_max) == approx(np.array([1, 1, 1], dtype=np.float32))
     assert tr.transform(obs_min) == approx(-1)
     assert tr.transform(obs_max) == approx(1)
